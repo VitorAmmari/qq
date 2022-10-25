a812e7b8c75c057a6aafd7e14dcfc5be2d832737
Vitor <vitorammari@hotmail.com>
2022-10-25 15:37:13 -0300

    caminhos html

--- src/app.js
+++ src/app.js
@@ -4,9 +4,35 @@ const PORT = 3333
 const User = require("../models/User")
 
 app.use(express.json())
+app.use(express.static(__dirname + "/css"))
+app.use(express.static(__dirname + "/images"))
 
-app.get("/", async(req, res) => {
-  res.send("Página Inicial")
+app.get("/home", async(req, res) => {
+  res.sendFile(__dirname + "/pages/login.html")
+})
+
+app.get("/index", async(req, res) => {
+  res.sendFile(__dirname + "/pages/index.html")
+})
+
+app.get("/index2", async(req, res) => {
+  res.sendFile(__dirname + "/pages/index-2.html")
+})
+
+app.get("/cadastrar", async(req, res) => {
+  res.sendFile(__dirname + "/pages/cadastro.html")
+})
+
+app.get("/crm", async(req, res) => {
+  res.sendFile(__dirname + "/pages/crm.html")
+})
+
+app.get("/alterar-senha", async(req, res) => {
+  res.sendFile(__dirname + "/pages/senha.html")
+})
+
+app.get("/pesquisar", async(req, res) => {
+  res.sendFile(__dirname + "/pages/pesquisa.html")
 })
 
 app.post("/cadastrar", async(req, res) => {
@@ -22,7 +48,8 @@ app.post("/cadastrar", async(req, res) => {
       mensagem: "Erro: Usuário não cadastrado com sucesso!"
     })
   })
-
+  
+  res.redirect("/cadastrar")
   res.send("Página Cadastrar")
 })
 
